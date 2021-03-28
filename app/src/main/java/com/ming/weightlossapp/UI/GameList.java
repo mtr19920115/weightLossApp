80934381925476368206f9323036d5e513a206c0
mtr19920115 <mtr19920115@163.com>
2021-03-27 23:47:16 -0700

    modified Gamelist activity

--- app/src/main/java/com/ming/weightlossapp/UI/GameList.java
+++ app/src/main/java/com/ming/weightlossapp/UI/GameList.java
@@ -2,6 +2,7 @@ package com.ming.weightlossapp.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,6 +15,7 @@ public class GameList extends AppCompatActivity {
     ListView gameList;
     Button back;
     Button refresh;
+    Button startNewGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +31,7 @@ public class GameList extends AppCompatActivity {
         gameList=(ListView) findViewById(R.id.gll_gameList);
         back=(Button) findViewById(R.id.glbt_back);
         refresh=(Button) findViewById(R.id.glbt_refresh);
+        startNewGame=(Button) findViewById(R.id.glbt_startNewGame);
     }
 
     private void initData(){
@@ -39,6 +42,15 @@ public class GameList extends AppCompatActivity {
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                finish();
+            }
+        });
+
+        startNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(GameList.this,JoinedGame.class);
+                startActivity(intent);
                 finish();
             }
         });
